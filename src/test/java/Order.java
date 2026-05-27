042a61eaa0d9a943c8f4704fd8a1775fe766231f
katherine lopez <codedbykatherine@gmail.com>
2026-05-27 14:43:40 -0400

    Refactor Order class to manage drink collection

--- src/test/java/Order.java
+++ src/test/java/Order.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Order {
+
+    //order owns drink list
+    ArrayList<Drink> drinks = new ArrayList<>();
+
+    // void does smt but give nothing in return
+    public void addDrink(Drink drink){
+        this.drinks.add(drink);
+        //the drinks list inside this order
+        //addDrink - method name
+        //Drink drink - method Drink accepts one drink object
+        //this - drink list from current order object
+        // add - take this one drink and add it to this order's drink list
+
+
+    }
+
 }
