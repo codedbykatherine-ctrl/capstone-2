6c4f8ebe2104674ea76455f8d174ecca92861bbd
katherine lopez <codedbykatherine@gmail.com>
2026-05-27 13:35:36 -0400

    Initialize taco shop project

--- src/test/java/Drink.java
+++ src/test/java/Drink.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Drink {
+    private DrinkSize size;
+
+    public void setDrinkSize( DrinkSize size ) {
+        this.size = size;
+    }
+
+    public DrinkSize getDrink() {
+        return this.size;
+    }
+
+    public double getPrice(){
+        return this.size.getPrice();
+    }
+ //   private  ArrayList<Drink> drinks = new ArrayList<>();
+
+    //public void addDrink(){
+       // this.drinks.add(Order);
+
+    Initialize taco shop project
+
+
+
 }
