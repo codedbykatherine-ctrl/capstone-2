24ba1c9fce5b07b98ed501ca6aba220b76e0351f
katherine lopez <codedbykatherine@gmail.com>
2026-05-27 17:58:53 -0400

    Add drink total calculation to Order

--- src/test/java/Order.java
+++ src/test/java/Order.java
@@ -10,11 +10,24 @@ public class Order {
         this.drinks.add(drink);
         //the drinks list inside this order
         //addDrink - method name
-        //Drink drink - method Drink accepts one drink object
+        //Drink  drink - method Drink accepts one drink object
         //this - drink list from current order object
         // add - take this one drink and add it to this order's drink list
 
 
     }
+    public double getDrinkTotal(){
+        double total = 0 ;
+
+        // this drink list inside THIS order
+        for (Drink drink : this.drinks ) { // for every drink in this orders list
+             total += drink.getPrice();// go through every Drink object in this.drinks one by one
+        }                              // add the price into the total
+        return total;
+    }
+
+
+
+
 
 }
