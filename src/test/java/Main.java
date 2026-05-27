4b197ebeff9f5acc423f891029624c7b6de3473e
katherine lopez <codedbykatherine@gmail.com>
2026-05-27 13:42:41 -0400

    Move drink pricing logic into DrinkSize enum

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Main {
+    public static void main(String[] args){
+
+        Drink drink1 = new Drink();
+        drink1.setSize("medium");
+
+
+        Order order1 = new Order();
+        order1.addName("k");
+
+       // Drink twisted = new Drink();
+       // Drink drink;
+        //twisted.addDrink(drink);
+
+
+        Drink twisted = new Drink();
+        twisted.setSize("Large");
+        ArrayList<Drink> drinks = new ArrayList<>();
+        drinks.add(twisted);
+
+
+
+//
+//
+//        System.out.println(blah);
+
+
+    }
 }
