d44d3fa36ba771c7de350020a8305c7c0df87f89
katherine lopez <codedbykatherine@gmail.com>
2026-05-27 13:40:05 -0400

    Add pricing to DrinkSize enum

--- src/test/java/DrinkSize.java
+++ src/test/java/DrinkSize.java
@@ -1,6 +1,4 @@
-public enum DrinkSize {
-
-    //stop invalid choices and organize related data
+public enum DrinkSize {    //stop invalid choices and organize related data
     // LOCKS THE CHOICES
     //CONTROLLED SET OF ALLOWED VALUES
     // SECRETLY TINY OBJECTS
@@ -12,7 +10,7 @@ public enum DrinkSize {
     //field to store parameter double
     private final double price;
 
-     //double parameter catches price
+    //double parameter catches price
     DrinkSize(double price) {
         this.price = price;
         //take incoming parameter store inside this objects field (price)
@@ -22,4 +20,6 @@ public enum DrinkSize {
         return price;
 
     }
+
+
 }
