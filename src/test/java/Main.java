e5c7795a387aa5249b7501aae524e51c01e1c398
katherine lopez <codedbykatherine@gmail.com>
2026-05-28 15:47:54 -0400

    add taco toppings Arrarylist and topping add method

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -5,33 +5,40 @@ public class Main {
 
         Drink drink1 = new Drink();
         drink1.setDrinkSize(DrinkSize.MEDIUM);
+        drink1.getPrice();
+        System.out.println(drink1.getPrice());
+        System.out.println(drink1.getDrinkSize());
 
 
-       Drink twisted = new Drink();
-       twisted.setDrinkSize(DrinkSize.LARGE);
 
-       Order mama = new Order();
-       mama.addDrink(twisted);
-       mama.addDrink(drink1);
-
-
-      /* System.out.println(drink1.getPrice());
-         System.out.println(twisted.getDrinkSize());
-        System.out.println(twisted.getPrice()); */
-
-        for (Drink drink : mama.getDrinks()){
-            System.out.println(drink.getDrinkSize() +
-                    " $" + drink.getPrice());
-        }
-        System.out.println( "Drinks Total: $ " + mama.getDrinkTotal());
-
-        Taco kat = new Taco();
-        kat.setTacoSize("medium");
-        kat.setTacoShell("corn ");
-
-
-        System.out.println(" Is this your Taco:: \n "
-                + kat.TacoSummary());
+//       Drink twisted = new Drink();
+//       twisted.setDrinkSize(DrinkSize.LARGE);
+//
+//       Order mama = new Order();
+//       mama.addDrink(twisted);
+//       mama.addDrink(drink1);
+//
+//       drink1.getPrice();
+//
+//
+//      /* System.out.println(drink1.getPrice());
+//         System.out.println(twisted.getDrinkSize());
+//        System.out.println(twisted.getPrice()); */
+//
+//        for (Drink drink : mama.getDrinks()){
+//            System.out.println(drink.getDrinkSize() +
+//                    " $" + drink.getPrice());
+//        }
+//        System.out.println( "Drinks Total: $ " + mama.getDrinkTotal());
+//
+//        Taco kat = new Taco();
+//        kat.setTacoSize("medium");
+//        kat.setTacoShell("corn ");
+//        kat.setTacoMeat("steak");
+//       kat.setTacoQuantity(4);
+//
+//        System.out.println(" Is this your Taco:: \n "
+//                + kat.tacoSummary());
 
 
 
