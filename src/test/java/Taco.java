90deffba769fc71693f7540efb8a6b8935be83bb
katherine lopez <codedbykatherine@gmail.com>
2026-05-27 23:03:43 -0400

    create basic taco structure

---
+++ src/test/java/Taco.java
@@ -0,0 +1,12 @@
+public class Taco {
+    private String size;
+    private String shell;
+
+public void setTacoSize (String size){
+    this.size = size;
+}
+public String getSize(){
+    return this.size;
+}
+
+}
