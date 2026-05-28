29c1bdecf68370b9094d1820183b99bb19c24a8e
katherine lopez <codedbykatherine@gmail.com>
2026-05-27 23:52:49 -0400

    implemented taco object meat and method testing

--- src/test/java/Taco.java
+++ src/test/java/Taco.java
@@ -1,22 +1,35 @@
 public class Taco {
     private String size;
     private String shell;
+    private String meat;
 
-public void setTacoSize (String size){
-    this.size = size;
-}
-public String getTacoSize(){
-    return this.size;
-}
-    public void setTacoShell (String shell){
+    public void setTacoSize(String size) {
+        this.size = size;
+    }
+
+    public String getTacoSize() {
+        return this.size;
+    }
+
+    public void setTacoShell(String shell) {
         this.shell = shell;
     }
-    public String getTacoShell(){
+
+    public String getTacoShell() {
         return this.shell;
     }
-    public String TacoSummary(){
-    return "Taco size: " + this.size +
-            " \n Taco shell:  " + this.shell;
+
+    public void setTacoMeat(String meat) {
+        this.meat = meat;
+    }
+    public String getTacoMeat(){
+        return this.meat;
+    }
+
+    public String tacoSummary() {
+        return "Taco size: " + this.size +
+                " \n Taco shell: " + this.shell +
+                "\n Taco meat selection: " + this.meat;
     }
 
 }
