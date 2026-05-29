978a4e135296cb0ee87ee1b238abec3cda1a5a99
katherine lopez <codedbykatherine@gmail.com>
2026-05-29 08:30:54 -0400

    add pricing logic to taco toppings enum

--- src/test/java/TacoToppings.java
+++ src/test/java/TacoToppings.java
@@ -1,16 +1,34 @@
 public enum TacoToppings {
-    EXTRA_MEAT,
-    EXTRA_CHEESE,
-    LETTUCE,
-    CILANTRO,
-    ONIONS,
-    TOMATOES,
-    JALAPENOS,
-    RADISHES,
-    PICO,
-    GUACAMOLE,
-    CORN;
+    EXTRA_MEAT(.50, 1.00, 1.50),
+    EXTRA_CHEESE(.30, .60, .90),
+    LETTUCE(0, 0, 0),
+    CILANTRO(0, 0, 0),
+    ONIONS(0, 0, 0),
+    TOMATOES(0, 0, 0),
+    JALAPENOS(0, 0, 0),
+    RADISHES(0, 0, 0),
+    PICO(0, 0, 0),
+    GUACAMOLE(0, 0, 0),
+    CORN(0, 0, 0),
+    private final double singlePrice;
+    private final double threeTacoPrice;
+    private final double burritoPrice;
 
 
+    TacoToppings(double singlePrice, double threeTacoPrice, double burritoPrice) {
+        this.singlePrice = singlePrice;
+        this.threeTacoPrice = threeTacoPrice;
+        this.burritoPrice = burritoPrice;
+    }
+    public double getPrice(String tacoStyle) {
+        if (tacoStyle.equalsIgnoreCase("Single")) {
+            return singlePrice;
+        } else if (tacoStyle.equalsIgnoreCase("3 taco")) {
+            return threeTacoPrice;
+        } else if (tacoStyle.equalsIgnoreCase("Burrito")) {
+            return burritoPrice;
+        }
+
+        return 0;
 
 }
