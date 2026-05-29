67513ca53366293fdd76fd09abe84ebe21fc5e1e
katherine lopez <codedbykatherine@gmail.com>
2026-05-29 03:20:52 -0400

    add scanner input flow for taco style selection

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,7 +1,45 @@
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        Taco express = new Taco();
+        Order kat = new Order();
+
+        System.out.println("What taco style would you like?");
+        String tacoStyle = scanner.nextLine();
+        // what user type gets stored in variable name tacoStyle
+
+        express.setTacoStyle(tacoStyle);
+
+        express.setTacoSize("medium");
+        express.setTacoShell("corn");
+        express.addTacoTopping("lettuce");
+        express.setTacoQuantity(4);
+
+        kat.addOrderTaco(express);
+
+        System.out.println(kat.orderSummary());
+        System.out.println("Order total: $" + kat.orderTacoTotal());
+
+        System.out.println("Is this your Taco:: \n"
+                + express.tacoSummary());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         Drink drink1 = new Drink();
         drink1.setDrinkSize(DrinkSize.MEDIUM);
@@ -11,39 +49,6 @@ public class Main {
 
 
 
-//       Drink twisted = new Drink();
-//       twisted.setDrinkSize(DrinkSize.LARGE);
-//
-//       Order mama = new Order();
-//       mama.addDrink(twisted);
-//       mama.addDrink(drink1);
-//
-//       drink1.getPrice();
-//
-//
-//      /* System.out.println(drink1.getPrice());
-//         System.out.println(twisted.getDrinkSize());
-//        System.out.println(twisted.getPrice()); */
-//
-//        for (Drink drink : mama.getDrinks()){
-//            System.out.println(drink.getDrinkSize() +
-//                    " $" + drink.getPrice());
-//        }
-//        System.out.println( "Drinks Total: $ " + mama.getDrinkTotal());
-//
-      Taco express = new Taco();
-      Order kat = new Order();
-        express.setTacoSize("medium");
-        express.addTacoTopping("lettuce");
-        express.setTacoQuantity(4);
-        kat.addOrderTaco(express);
-        System.out.println(kat.orderSummary());
-//        kat.setTacoShell("corn ");
-//        kat.setTacoMeat("steak");
-//       kat.setTacoQuantity(4);
-//
-       System.out.println(" Is this your Taco:: \n "
-                + express.tacoSummary());
 
 
 
